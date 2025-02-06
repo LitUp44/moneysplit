baf49c7b17e3ce09fbb71285fd819f6e24446deb
LitUp44 <litwiller.h@gmail.com>
2025-02-06 15:34:52 +1100

    Update codefile.py

--- codefile.py
+++ codefile.py
@@ -29,8 +29,8 @@ def calculate_proportional_expenses(expenses, income1, income2):
     #percent1 = (income1 / total_income) * 100
     #percent2 = (income2 / total_income) * 100
     expense_total = (expenses * total_percent) / 100
-    remaining1 = income1 - expense1
-    remaining2 = income2 - expense1
+    remaining1 = income1 - expense_total
+    remaining2 = income2 - expense_total
     return percent1, percent2, expense_total, remaining1, remaining2
 
 # Function to calculate split by bills
@@ -76,8 +76,8 @@ elif option == 'Complete share':
     st.write(f"You each have ${remaining:.2f} left after expenses.")
 
 elif option == 'Proportional expenses':
-    percent1, percent2, expense1, remaining1, remaining2 = calculate_proportional_expenses(expenses, income1, income2)
-    st.write(f"You both pay {percent1:.2f}% of your income to expenses, ${expense1:.2f}.")
+    percent1, percent2, expense_total, remaining1, remaining2 = calculate_proportional_expenses(expenses, income1, income2)
+    st.write(f"You both pay {percent1:.2f}% of your income to expenses, ${expense_total:.2f}.")
     st.write(f"Person 1 has ${remaining1:.2f} left after expenses.")
     st.write(f"Person 2 has ${remaining2:.2f} left after expenses.")
 
