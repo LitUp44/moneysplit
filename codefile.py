3d1c2a3d7ba7c82ad4d8d2574e77d171502d049c
LitUp44 <litwiller.h@gmail.com>
2025-02-09 21:14:34 +1100

    Update codefile.py

--- codefile.py
+++ codefile.py
@@ -153,10 +153,6 @@ elif option == 'Split by bills':
         <p>This is an interesting one that a lot of people fall into. They’ll start divvying up payments – one person pays for the <strong>utilities</strong> and car payments, the other the <strong>mortgage</strong> and the <strong>groceries</strong>. This can work if both people are happy with the arrangement and feel that the split is roughly appropriate according to their incomes or beliefs.</p>
 
         <p>The challenge is that both <strong>expenses</strong> and <strong>incomes</strong> change on a very consistent basis. This means that you can quickly end up with a situation that looks very different than the one you started with. It’s a lot of effort to be constantly renegotiating how you should be splitting everything up.</p>
-
-        <p>There are of course varieties to each of these methods! You might do <strong>50/50</strong> generally but one partner pays more for <strong>rent</strong>, for example. OR you combine expenses fully but you contribute to <strong>investment</strong> / <strong>pension accounts</strong> at different rates.</p>
-
-        <p>Whatever works for you – the important part is <strong>DECIDING together</strong> and not falling into a trap of just doing whatever is easiest at the time.</p>
     """, unsafe_allow_html=True)
     st.image("Split by bills.png", width=700)  # Adjust image path as needed
 
